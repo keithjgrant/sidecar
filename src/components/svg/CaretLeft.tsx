140dc5c2da87407da869c858e8d1f1363ea184cb
Keith Grant <barkeep@keithjgrant.com>
2026-07-24 14:14:54 -0700

    typescript batch - icons

--- src/components/svg/CaretLeft.tsx
+++ src/components/svg/CaretLeft.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-export default function CaretLeft({ title }) {
+interface CaretLeftProps {
+  title?: string;
+}
+
+export default function CaretLeft({ title }: CaretLeftProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
