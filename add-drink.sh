0f2e5055f599322d71566079597afd7b5588299f
Keith Grant <barkeep@keithjgrant.com>
2026-07-24 16:30:32 -0700

    add-drink.sh bug fixes/robustness

--- add-drink.sh
+++ add-drink.sh
@@ -1,19 +1,27 @@
 #!/bin/zsh
 
-NAME=$1
-SLUG=$(echo $NAME | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z)
-DATE=$(date "+%Y-%m-%dT%H:%m:%S-08:00")
-FILE="src/pages/drinks/${SLUG}.md"
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-if [ -f $FILE ]; then
+NC='\033[0m'
+
+if [ -z "$1" ]; then
+  echo -e "${RED}Usage: ./add-drink.sh \"Drink Name\"${NC}"
+  exit 1
+fi
+
+NAME="$1"
+SLUG=$(echo "$NAME" | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z | sed 's/^-//;s/-$//')
+DATE=$(date "+%Y-%m-%dT%H:%M:%S%z" | sed 's/\(..\)$/:\1/')
+FILE="src/pages/drinks/${SLUG}.md"
+
+if [ -f "$FILE" ]; then
   echo -e "${RED}Cannot create '$FILE': already exists${NC}"
   exit 1
 fi
 
-echo -e "Creating: ${GREEN}src/pages/drinks/${SLUG}.md${NC}"
-sed "s/title:/title: \"${NAME}\"/" DRINK_TEMPLATE.md > ${FILE}
-sed -i '' "s/path:/path: \"\/drinks\/${SLUG}\"/" $FILE
-sed -i '' "s/^date:/date: ${DATE}/" $FILE
+ESCAPED_NAME=$(echo "$NAME" | sed 's/[&/\]/\\&/g')
+
+echo -e "Creating: ${GREEN}${FILE}${NC}"
+sed "s/title:/title: \"${ESCAPED_NAME}\"/" DRINK_TEMPLATE.md > "$FILE"
+sed -i '' "s/path:/path: \"\/drinks\/${SLUG}\"/" "$FILE"
+sed -i '' "s/^date:/date: ${DATE}/" "$FILE"
